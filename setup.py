c06ad84bea67da884272c40862ee68028af8d5a0
Young Trep <64561735+YoungTrep@users.noreply.github.com>
2021-08-22 04:49:36 +0100

    Initial Release

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ufcpy',
+    version='0.9.9',
+    author='youngtrep',
+    author_email='youngtrep.business@gmail.com',
+    description='A fast and easy way to access the UFC roster',
+    long_description=long_description,
+    url='https://github.com/YoungTrep/ufcpy',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+    ],
+    license='MIT',
+    keywords=['ufc', 'mma', 'mixed martial arts', 'fighting', 'fighters', 'ufc-api', 'mma-api'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    python_requires='>=3.6'
+)
---
+++ ufcpy.py
@@ -0,0 +1,99 @@
+import re
+import math
+
+import urllib.request
+
+from bs4 import BeautifulSoup as bs
+
+class UFCPyError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    
+    def __str__(self):
+        return repr(self.msg)
+
+class Fighter(object):
+    def __init__(self):
+        self.base_url = "http://ufc.com/athlete"
+        self.parsed_url = None
+        self.name = None
+        self.nick = None
+        self.age = None
+        self.record = None
+        self.wins = None
+        self.losses = None
+        self.draws = None
+        self.rank = None
+        self.hometown = None
+        self.active = None
+        self.image_url = None
+        self.height_inches = None
+    
+    def get_fighter(self, fighter):
+        url = str(fighter).lower().replace(" ", "-")
+        page = urllib.request.urlopen(f'{self.base_url}/{url}')
+        self.parsed_url = bs(page, 'html.parser')
+        self._get_attributes(fighter)
+
+    def _get_attributes(self, fighter):
+        self.name = self._parse_html('div', _class='c-hero--full__headline is-large-text').get_text().strip()
+        self.age = self._parse_html('div', _class='field field--name-age field--type-integer field--label-hidden field__item').get_text()
+        self.nick = self._parse_html('div', _class='field field-name-nickname').get_text().strip('/"')
+        record = re.findall(r'\d+', self._parse_html('div', _class='c-hero__headline-suffix tz-change-inner').get_text().split('•')[1])
+        self.record = f'{record[0]}-{record[1]}-{record[2]}'
+        self.wins = record[0]
+        self.losses = record[1]
+        self.draws = record[2]
+        self.hometown = self._parse_html('div', _class='c-bio__field c-bio__field--border-bottom-small-screens').get_text().replace('Hometown', '').strip()
+        self.active = self._parse_html('div', _class="c-bio__text").get_text()
+        self._weight_class()
+        self._get_rank()
+        self.image_url = self._parse_html('div', _class='c-bio__image--mobile').find_next().get('src')
+        self.height_inches = self._parse_html('div', _class='c-bio__row--3col').get_text().split()[3].replace('.00', '')
+        self._height_feet()
+
+    def _parse_html(self, _element, _id=None, _class=None):
+        parsed = self.parsed_url.find(_element) or ""
+        if _id is not None:
+            parsed = self.parsed_url.body.find(_element, {'id': _id})
+        if _class is not None:
+            parsed = self.parsed_url.body.find(_element, {'class': _class})
+        if _id is not None and _class is not None:
+            parsed = self.parsed_url.body.find(_element, {'id': _id, 'class': _class})
+        return parsed
+    
+    def _get_rank(self):
+        rank = self._parse_html('div', _class='c-hero__headline-suffix tz-change-inner').get_text().split('•')[0].split('\n')
+        if 'Champion' in rank[1]:
+            self.rank = rank[1].strip()
+        elif rank[1].strip().startswith('#'):
+            self.rank = f'{rank[1].strip()} {rank[2].strip()}'
+        else:
+            self.rank = f'Unranked {rank[1].strip()} {rank[2].strip()}'
+
+    def _weight_class(self):
+        self.weight = self._parse_html('div', _class='c-bio__row--3col').get_text().split()[5].replace('.00', '')
+        if self.weight == '125':
+            self.weight_class = 'Flyweight'
+        if self.weight == '135':
+            self.weight_class = 'Bantamweight'
+        if self.weight == '145':
+            self.weight_class = 'Featherweight'
+        if self.weight == '155':
+            self.weight_class = 'Lightweight'
+        if self.weight == '170':
+            self.weight_class = 'Welterweight'
+        if self.weight == '185':
+            self.weight_class = 'Middleweight'
+        if self.weight == '205':
+            self.weight_class = 'Light Heavyweight'
+        if self.weight == '250':
+            self.weight_class = 'Heavyweight'
+
+    def _height_feet(self):
+        height_feet = round(int(self.height_inches) / 12, 1)
+        new_height = str(height_feet).replace('.', ' ').split(' ')
+        if new_height[1] == '0':
+            self.height_feet = f'{new_height[0]} foot'
+        else:
+            self.height_feet = f'{new_height[0]} foot {new_height[1]} inch'
