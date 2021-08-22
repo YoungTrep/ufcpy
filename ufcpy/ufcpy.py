0adb370557a50bc6cafba3ab935caaa48273faeb
Young Trep <64561735+YoungTrep@users.noreply.github.com>
2021-08-22 15:57:47 +0100

    yeah effort setting up git

--- ufcpy/ufcpy.py
+++ ufcpy/ufcpy.py
@@ -28,6 +28,15 @@ class Fighter(object):
         self.active = None
         self.image_url = None
         self.height_inches = None
+        self.striking_accuracy = None
+        self.sig_strikes_landed = None
+        self.sig_strikes_attempted = None
+        self.grappling_accuracy = None
+        self.landed_takedowns = None
+        self.attempted_takedowns = None
+        self.debut = None
+        self.reach = None
+        self.leg_reach = None
     
     def get_fighter(self, fighter):
         url = str(fighter).lower().replace(" ", "-")
@@ -51,15 +60,33 @@ class Fighter(object):
         self.image_url = self._parse_html('div', _class='c-bio__image--mobile').find_next().get('src')
         self.height_inches = self._parse_html('div', _class='c-bio__row--3col').get_text().split()[3].replace('.00', '')
         self._height_feet()
+        self.striking_accuracy = self._parse_html('div', _class='l-overlap-group__item--odd').get_text().strip().split()[3]
+        self.sig_strikes_landed = self._parse_html('div', _class='l-overlap-group__item--odd').get_text().strip().split()[8].replace('Landed', '')
+        self.sig_strikes_attempted = self._parse_html('div', _class='l-overlap-group__item--odd').get_text().strip().split()[11].replace('Attempted', '')
+        self.grappling_accuracy = self._parse_html('div', _class='l-overlap-group__item--even').get_text().strip().split()[3]
+        self.landed_takedowns = self._parse_html('div', _class='l-overlap-group__item--even').get_text().strip().split()[7].replace('Landed', '')
+        self.attempted_takedowns = self._parse_html('div', _class='l-overlap-group__item--even').get_text().strip().split()[9].replace('Attempted', '')
+        second_stats = self._parse_html('div', _class='c-bio__row--3col', _find_all=True)[1].text.split()
+        self.debut = f'{second_stats[2]} {second_stats[3]} {second_stats[4]}'
+        self.reach = second_stats[6]
+        self.leg_reach = second_stats[9]
 
-    def _parse_html(self, _element, _id=None, _class=None):
+    def _parse_html(self, _element, _id=None, _class=None, _find_all: bool = False):
         parsed = self.parsed_url.find(_element) or ""
-        if _id is not None:
-            parsed = self.parsed_url.body.find(_element, {'id': _id})
-        if _class is not None:
-            parsed = self.parsed_url.body.find(_element, {'class': _class})
-        if _id is not None and _class is not None:
-            parsed = self.parsed_url.body.find(_element, {'id': _id, 'class': _class})
+        if _find_all == False:
+            if _id is not None:
+                parsed = self.parsed_url.body.find(_element, {'id': _id})
+            if _class is not None:
+                parsed = self.parsed_url.body.find(_element, {'class': _class})
+            if _id is not None and _class is not None:
+                parsed = self.parsed_url.body.find(_element, {'id': _id, 'class': _class})
+        else:
+            if _id is not None:
+                parsed = self.parsed_url.body.find_all(_element, {'id': _id})
+            if _class is not None:
+                parsed = self.parsed_url.body.find_all(_element, {'class': _class})
+            if _id is not None and _class is not None:
+                parsed = self.parsed_url.body.find_all(_element, {'id': _id, 'class': _class})
         return parsed
     
     def _get_rank(self):
