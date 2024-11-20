225e1abe3328d37c6328a5b393c3cc7abce99ac3
YoungTrep <youngtrep.business@gmail.com>
2024-11-20 21:57:00 +0000

    fix install requires

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name='ufcpy',
     version='3.0.0',
     author='youngtrep',
@@ -11,10 +11,11 @@ setuptools.setup(
     description='A fast and easy way to access the UFC roster',
     long_description=long_description,
     url='https://github.com/YoungTrep/ufcpy',
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'bs4',
-        'requests'
+    packages=find_packages(),
+    install_requires= [
+        'beautifulsoup4 >= 4.12.3',
+        'requests >= 2.32.3', 
+        'lxml >= 5.3.0'
     ],
     license='MIT',
     keywords=['ufc', 'mma', 'mixed martial arts', 'fighting', 'fighters', 'ufc-api', 'mma-api'],
@@ -30,5 +31,5 @@ setuptools.setup(
         'Topic :: Utilities',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.8'
 )
